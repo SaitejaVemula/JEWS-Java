203a380c863bc7fe30c5a25ca853e887b63c3b02
SaitejaVemula <saitejavemula@gmail.com>
2020-03-18 23:18:39 +0530

    Made a program on Factorial
    Created Factorial.java program

---
+++ Holidays Assignments/Factorial.java
@@ -0,0 +1,33 @@
+// Importing Scanner class from "util" package
+import java.util.Scanner;
+
+public class Factorial
+{
+  // this static function returns the factorial of the given number
+  public static long factorial(byte num) {
+    long result = 1;
+
+    if(num == 0 || num == 1) {
+      // returns 1 if the number is 0 or 1
+      return 1;
+    }
+
+    // calculating the factorial by using a for loop
+    for (int i = 1; i <= num; i++) {
+      result *= i;
+    }
+
+    // returns the factorial
+    return result;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter a number to find its factorial :  ");
+    byte num = sc.nextByte();
+
+    System.out.println("Factorial of " + num + " is " + factorial(num));
+  }
+
+}
