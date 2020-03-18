bbf06e3c1d54b1c6f580ed6c69a064bd23ed962d
SaitejaVemula <saitejavemula@gmail.com>
2020-03-18 11:00:32 +0530

    Added comments to yesterday's code.

--- Day-10/Fibonacci.java
+++ Day-10/Fibonacci.java
@@ -1,26 +1,37 @@
+//Importing the Scanner class
 import java.util.Scanner;
+
+// Created a class named Fibonacci
 public class Fibonacci
 {
   public static void main(String[] args)
   {
     int n1 = 0, n2 = 1, sum = 0, num, count = 0;
+
+    //Creating an object named "sc" to user Scanner
     Scanner sc = new Scanner(System.in);
 
+    //Output statement
     System.out.println("*** FIBONACCI NUMBERS ***");
     System.out.print("Enter the number of fibbonacci numbers you want me to print :  ");
+    
+    // Reading the input from the user and storing it in "num"
     num = sc.nextInt();
     System.out.println();
 
+    // Printing the first number in the series
     if (num > 0) {
       System.out.print(n1 + "\t\t");
       count++;
     }
     
+    // Printing the second number in the series
     if (num > 1) {
       System.out.print(n2 + "\t\t");
       count++;
     }
     
+    // Printing the numbers in the series till the entered number
     if (num > 2) {
       while(num > count) {
         sum = n1 + n2;
--- Day-10/PrimeNumber.java
+++ Day-10/PrimeNumber.java
@@ -1,6 +1,12 @@
+//Importing the Scanner class
 import java.util.Scanner;
+
+// Created a class named PrimeNumber
 public class PrimeNumber {
 
+  // Created a static method named "isAPrime"
+  // It returns true if passed number is prime number
+  // It returns false if its a composite number
   public static boolean isAPrime(int num) {
     for(int i = 2; i <= num / 2; i++) {
       if(num % i == 0) {
@@ -11,20 +17,40 @@ public class PrimeNumber {
   }
 
   public static void main(String[] args) {
+    // Created a scanner object named "sc"
     Scanner sc = new Scanner(System.in);
+
+    // Asking the user for input
     System.out.print("Enter a number to check if its a prime or not :  ");
+    
+    // Storing the given number in "num"
     int num = sc.nextInt();
     
     if(num == 0) {
+
+      // Printing the entered number is zero if num equals to zero
       System.out.println("Entered number is zero.");
+
     } else if (num == 1) {
+
+      // If entered number is 1, then printing that its neither prime nor composite
       System.out.println("1 is neither prime not compisite");
+
     } else {
+
+      // Calling the "isAPrime" static method and passing it the entered number.
       if(isAPrime(num)) {
+
+        // if it returns true, it means it a prime number. Hence the output 
         System.out.println(num + " is a prime number");
+        
       } else {
+
+        // if it returns true, it means it a composite number (not a prime number). Hence the output 
         System.out.println(num + " is a composite number");
+
       }
+
     }
   }
 }
