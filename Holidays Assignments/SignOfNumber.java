bc71275ca358a99a812e37d0127023df2319f25c
SaitejaVemula <saitejavemula@gmail.com>
2020-03-19 23:52:44 +0530

    Update SignOfNumber.java
    Added comments to "SignOfNumber.java"

--- Holidays Assignments/SignOfNumber.java
+++ Holidays Assignments/SignOfNumber.java
@@ -1,7 +1,9 @@
+// importing scanner class to take input from the user.
 import java.util.Scanner;
 
 public class SignOfNumber {
 
+  // created a static void function, that prints the output (i.e., sign of the number)
   public static void signOfNumber(int num) {
     if(num == 0) {
       System.out.println("Number is zero, its neither positive, nor negative.");
@@ -13,11 +15,15 @@ public class SignOfNumber {
   } 
 
   public static void main(String[] args) {
+    // Created a scanner object named "sc"
     Scanner sc = new Scanner(System.in);
 
+    // asking user for the number(input)
     System.out.print("Enter a number to know its sign :  ");
+    // storing the entered number in "num"
     int num = sc.nextInt();
 
+    // calling the signOfNumber() method
     signOfNumber(num);
 
   }
