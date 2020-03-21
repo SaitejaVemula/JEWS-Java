689fdb3df880bd37744ae3da365f097b814a68c8
SaitejaVemula <saitejavemula@gmail.com>
2020-03-21 15:59:03 +0530

    Added comments to the ArithmeticOperations.java

--- Holidays Assignments/ArithmeticOperations.java
+++ Holidays Assignments/ArithmeticOperations.java
@@ -1,23 +1,30 @@
+// Importing the Scanner class from "util" package.
 import java.util.Scanner;
 
+// Created a class named "ArithmeticOperations"
 public class ArithmeticOperations {
-  
+
+  // A static method that returns the sum of inputted two numbers
   public static void add(int a, int b) {
     System.out.println("Sum of " + a + " and " + b + " is " + (a + b));
   }
-
+  
+  // A static method that returns the difference of inputted two numbers
   public static void subtract(int a, int b) {
     System.out.println("Difference of " + a + " and " + b + " is " + (a + b));
   }
-
+  
+  // A static method that returns the product of inputted two numbers
   public static void multiply(int a, int b) {
     System.out.println("Product of " + a + " and " + b + " is " + (a * b));
   }
-
+  
+  // A static method that returns the division of inputted two numbers
   public static void divide(int a, int b) {
     System.out.println("Division of " + a + " by " + b + " is " + (a * 1.0f / b));
   }
-
+  
+  // A static method that calls appropriate arithmetic method based on the inputted sign.
   public static void performCalculation(int a, int b, char c) {
     
     switch(c) {
@@ -27,11 +34,16 @@ public class ArithmeticOperations {
       case '/' : divide(a, b); break;
       default : System.out.println("You've entered wrong choice");;
     }
-    
+
   }
+
+  // Main Method
   public static void main(String[] args) {
     
+    // Created a Scanner object named "sc"
     Scanner sc = new Scanner(System.in);
+
+    // Reading the operation sign
     System.out.println("Enter ");
     System.out.println("'+' for Addition");
     System.out.println("'-' for Substraction");
@@ -40,12 +52,15 @@ public class ArithmeticOperations {
     System.out.println("Enter the sign :  ");
     char c = sc.nextLine().charAt(0);
 
+    // Reading two integer values to perform the above operation.
     System.out.print("Enter two integer values :  ");
     int a = sc.nextInt();
     int b = sc.nextInt();
 
+    // closing the Scanner object. 
     sc.close();
 
+    // Making a method call and passing the user inputs.
     performCalculation(a,b,c);
 
   }
