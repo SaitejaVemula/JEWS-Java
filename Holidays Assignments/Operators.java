223cdec780d1ee8a775fe38f2a8d36b1e99475ab
SaitejaVemula <saitejavemula@gmail.com>
2020-03-28 00:17:30 +0530

    Formatted Operators.java properly

--- Holidays Assignments/Operators.java
+++ Holidays Assignments/Operators.java
@@ -1,98 +1,87 @@
-/*
-  6.   Java Program to Calculate Simple Interest 
-  7.   Java Program to Calculate Compound Interest
-  9.   Java Program to check Leap Year
-11. Swaping of two Numbers
-13. Write a programme to find unary Operator and Binary Operators
-14. To check the Logical Operators
-15. To check the Assignment Operators
-16. Terinary Operator
-17. Write a programme to print 1 to 10 numbers To print Forward Direction and Backword Direction Using While loop and do while loop
-20. Write a programme to find the DD-MM-YY using switch statement
-
-*/
 import java.util.Scanner;
 
 public class Operators {
 
+  // This method prints the type/category of the given operator
   public static void operatorName(String sign) {  
-
     switch(sign) {
-      case "+" :
-      case "-" :
-      case "*" :
-      case "/" :
-      case "%" : System.out.println(sign + " is an Arithmetic operator"); break;
+      case   "+" :
+      case   "-" :
+      case   "*" :
+      case   "/" :
+      case   "%" : System.out.println(sign + " is an Arithmetic operator"); break;
 
-      case "=" : 
-      case "+=" : 
-      case "-=" : 
-      case "*=" : 
-      case "/=" : 
-      case "%=" : System.out.println(sign + " is an Assignment operator"); break;
+      case  "++" : System.out.println(sign + " is a pre/post increment operator"); break;
+      case  "--" : System.out.println(sign + " is a pre/post decrement operator"); break;
+
+      case   "=" : 
+      case  "+=" : 
+      case  "-=" : 
+      case  "*=" : 
+      case  "/=" : 
+      case  "%=" : System.out.println(sign + " is an Assignment operator"); break;
       
-      case "==" : 
-      case "!=" :
-      case ">" :
-      case ">=" :
-      case "<" :
-      case "<=" : System.out.println(sign + " is a Relational operator"); break;
+      case  "==" : 
+      case  "!=" :
+      case   ">" :
+      case  ">=" :
+      case   "<" :
+      case  "<=" : System.out.println(sign + " is a Relational operator"); break;
 
-      case "&&" : 
-      case "||" :  System.out.println(sign + " is a Logical operator"); break;
+      case  "&&" : 
+      case  "||" : System.out.println(sign + " is a Logical operator"); break;
       
-      case "&" : 
-      case "|" : 
-      case "^" : 
-      case "~" :  System.out.println(sign + " is a Bitwise operator"); break;
+      case   "&" : 
+      case   "|" : 
+      case   "^" : 
+      case   "~" : System.out.println(sign + " is a Bitwise operator"); break;
       
-      case "<<" : 
-      case ">>" : 
-      case ">>>" :  System.out.println(sign + " is a Shift operator"); break;
+      case  "<<" : 
+      case  ">>" : 
+      case ">>>" : System.out.println(sign + " is a Shift operator"); break;
 
-    }
-
+      default    : System.out.println("Please enter a valid operator"); 
+    } 
   }
-
+  
+  // This method prints the no of operands it needs
   public static void noOfOperands(String sign) {
-    
     switch(sign) {
-      case "+" : 
-      case "-" : System.out.println(sign + " is both unary and binary operator"); break;
+      case   "+" : 
+      case   "-" : System.out.println(sign + " is both unary and binary operator"); break;
 
-      case "~" : 
-      case "!" : 
-      case "++" : 
-      case "--" : System.out.println(sign + " is a unary operator"); break;
+      case   "~" : 
+      case   "!" : 
+      case  "++" : 
+      case  "--" : System.out.println(sign + " is a unary operator"); break;
       
-      case "*" : 
-      case "/" : 
-      case "%" : 
-      case "=" : 
-      case "+=" : 
-      case "-=" : 
-      case "*=" : 
-      case "/=" : 
-      case "%=" : 
-      case ">" : 
-      case ">=" : 
-      case "<" : 
-      case "<=" : 
-      case "==" : 
-      case "!=" : 
-      case "||" : 
-      case "|" : 
-      case "&" : 
-      case "^" : 
-      case "<<" : 
-      case ">>" : 
+      case   "*" : 
+      case   "/" : 
+      case   "%" : 
+      case   "=" : 
+      case  "+=" : 
+      case  "-=" : 
+      case  "*=" : 
+      case  "/=" : 
+      case  "%=" : 
+      case   ">" : 
+      case  ">=" : 
+      case   "<" : 
+      case  "<=" : 
+      case  "==" : 
+      case  "!=" : 
+      case  "||" : 
+      case   "|" : 
+      case   "&" : 
+      case   "^" : 
+      case  "<<" : 
+      case  ">>" : 
       case ">>>" : 
-      case "&&" : System.out.println(sign + " is a binary operator"); break;
+      case  "&&" : System.out.println(sign + " is a binary operator"); break;
 
-      case "?:" : System.out.println(sign + " is a ternary operator"); break;
+      case  "?:" : System.out.println(sign + " is a ternary operator"); break;
 
-      default: System.out.println("You've entered an incorrect operator");
-      
+      default    : System.out.println("You've entered an incorrect operator");
     }
   }
 
