8c09ee9c6b4eb0e88f7bc47026b0aed2be465acd
SaitejaVemula <saitejavemula@gmail.com>
2020-03-29 23:59:14 +0530

    Ternary

---
+++ Holidays Assignments/TernaryOperator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class TernaryOperator {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter your age :  ");
+    byte age = sc.nextByte();
+
+    boolean isAnAdult = false;
+    isAnAdult = age >= 18 ? true : false;
+
+    if(isAnAdult) {
+      System.out.println("You are an adult");
+    } else {
+      System.out.println("You are not an adult");
+    }
+  }
+}
