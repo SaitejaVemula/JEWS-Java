ca52703d86a157934254b0829e3012be627a25c3
SaitejaVemula <saitejavemula@gmail.com>
2020-03-31 22:54:43 +0530

    Buliding the logic for the commands and also iterating the whole thing

--- Holidays Assignments/Fight/Game.java
+++ Holidays Assignments/Fight/Game.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 
 public class Game {
 
-  byte MAX_HEALTH = 100;
-  byte MAX_SHIELD = 50;
+  static byte MAX_HEALTH = 100;
+  static byte MAX_SHIELD = 50;
 
-  byte user_health = MAX_HEALTH;
-  byte comp_health = MAX_HEALTH;
+  static byte user_health = MAX_HEALTH;
+  static byte comp_health = MAX_HEALTH;
 
-  byte user_shield = 0;
-  byte comp_shield = 0;
+  static byte user_shield = 0;
+  static byte comp_shield = 0;
 
   // public static byte punch() {
 
@@ -22,6 +22,7 @@ public class Game {
 
   public static void end() {
     System.out.println("You've entered \"end\". Quitting.... ");
+    System.exit(0);
   }
 
 
@@ -33,31 +34,60 @@ public class Game {
 
     Scanner sc = new Scanner(System.in);
     Random rd = new Random();
-    System.out.print("Please enter your name :  ");
-    String user_name = sc.nextLine();
-    System.out.print("Please enter your opponent's name :  ");
-    String comp_name = sc.nextLine();
+    // System.out.print("Please enter your name :  ");
+    // String user_name = sc.nextLine();
+    String user_name = "Sai";
+    // System.out.print("Please enter your opponent's name :  ");
+    // String comp_name = sc.nextLine();
     String input;
     boolean isValidInput = false;
 
     do {
 
-      System.out.print(name + ", select \"punch\", \"defend\" or \"end\" :  ");
-      input = sc.nextLine().toLowerCase();
-      
-      isValidInput = (input.equals("punch") || input.equals("defend") || input.equals("end")) ? true : false ;
+      do {
 
-      if(!isValidInput) {
-        System.out.println("You've entered wrong option. Please try again.");
+        System.out.print(user_name + ", select \"punch\", \"defend\" or \"end\" :  ");
+        input = sc.nextLine().toLowerCase();
+        
+        isValidInput = (input.equals("punch") || input.equals("defend") || input.equals("end")) ? true : false ;
+
+        if(!isValidInput) {
+          System.out.println("You've entered wrong option. Please try again.");
+        }
+
+      } while (!isValidInput);
+
+      switch(input) {
+        case "punch"  : byte amt = (byte) rd.nextInt(50);
+                        byte rem_health = 0;
+                        if(comp_shield > 0) {
+                          rem_health = (byte) (comp_shield - amt);
+                          comp_shield -= amt;
+                          if(comp_shield < 0) {
+                            comp_shield = 0;
+                          }
+                        }
+                        if(rem_health<0) {
+                          comp_health -= rem_health;
+                        } else {
+                          comp_health -= amt;
+                        }
+                        if(comp_health<0) {
+                          comp_health = 0;
+                        }
+                        break;
+        case "defend" : comp_shield += (byte) rd.nextInt(30);
+                        if(comp_shield > 50) {
+                          comp_shield = MAX_SHIELD;
+                        }
+                        break;
+        case "end"    : end(); break;
       }
 
-    } while (!isValidInput);
-
-    switch(input) {
-      case "punch"  : health -= punch(); break;
-      case "defend" : shield += defend(); break;
-      case "end"    : end(); break;
-    }
+      System.out.println("Computer's health now :  " + comp_health);
+      System.out.println("Computer's shield now :  " + comp_shield);
+    
+    } while(comp_health > 0);
     
   }
 }
